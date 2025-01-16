40caf3ad85fd3ca33ce4a7023f339971b8a09047
pjy <1009514534@qq.com>
2025-01-16 11:16:08 +0800

    docs: 更新 README 文件，添加 User-Agent 设置注意事项；feat: 新增 server/app.js 文件以支持 API 代理

---
+++ server/app.js
@@ -0,0 +1,60 @@
+const process = require('node:process')
+const express = require('express')
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+const app = express()
+
+// 环境变量
+const env = {
+  VITE_APP_STORE_API_PREFIX: '/appleapi',
+  VITE_APP_STORE_URL: 'https://apps.apple.com',
+  VITE_MT_SHOP_STATIC_API_PREFIX: '/mtstaticapi',
+  VITE_MT_SHOP_STATIC_URL: 'https://static.moutai519.com.cn',
+  VITE_MT_APP_API_PREFIX: '/mtappapi',
+  VITE_MT_APP_API_URL: 'https://app.moutai519.com.cn',
+}
+
+// app.use(express.static('dist')); // 假设你的 Vite 构建输出在 dist 目录, 按需修改
+
+// 配置代理
+app.use(
+  env.VITE_APP_STORE_API_PREFIX,
+  createProxyMiddleware({
+    target: env.VITE_APP_STORE_URL,
+    changeOrigin: true,
+    pathRewrite: path =>
+      path.replace(new RegExp(`^${env.VITE_APP_STORE_API_PREFIX}`), ''),
+  }),
+)
+
+app.use(
+  env.VITE_MT_SHOP_STATIC_API_PREFIX,
+  createProxyMiddleware({
+    target: env.VITE_MT_SHOP_STATIC_URL,
+    changeOrigin: true,
+    pathRewrite: path =>
+      path.replace(new RegExp(`^${env.VITE_MT_SHOP_STATIC_API_PREFIX}`), ''),
+  }),
+)
+
+app.use(
+  env.VITE_MT_APP_API_PREFIX,
+  createProxyMiddleware({
+    target: env.VITE_MT_APP_API_URL,
+    changeOrigin: true,
+    headers: {
+      // 浏览器控制台中出现 Refused to set unsafe header "User-Agent" 报错是浏览器的安全策略导致，
+      // 前端 axios 虽然设置了 UA，但是基于这个安全策略的原因，浏览器会自动修改 UA 为浏览器的 UA。
+      // 所以需要在这里重新设置 UA，避免在茅台 APP 接口中出现浏览器的 UA。
+      'User-Agent': 'iOS;16.3;Apple;?unrecognized?',
+    },
+    pathRewrite: path =>
+      path.replace(new RegExp(`^${env.VITE_MT_APP_API_PREFIX}`), ''),
+  }),
+)
+
+// 启动服务器
+const PORT = process.env.PORT || 12999
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
